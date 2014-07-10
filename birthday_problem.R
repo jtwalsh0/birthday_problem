7d9b8d99b7ce458e479d4b5748e09c2ab33cf44c
Joe Walsh <j.thomas.walsh@gmail.com>
2014-07-10 18:16:59 -0500

    Update birthday_problem.R
    I created a function that the user can call

--- birthday_problem.R
+++ birthday_problem.R
@@ -12,35 +12,51 @@
 # set seed
 set.seed(1234)
 
-# size of the group
-n <- 63
 
-# how many random samples to try
-n_trials <- 100000
+## Create birthday function
+birthday_problem <- function(n = 23,                      # size of the group
+                             min_shared_birthdays = 1,    # minimum number of shared birthdays (default: at least one shared birthday)
+                             min_people_sharing = 2,      # minimum number of people sharing the birthday (e.g. at least two people sharing a birthday)
+                             n_trials = 10000){           # how many random samples to try
 
-# generate n_trials samples
-data <- sample(x=1:365, size=n*n_trials, replace=TRUE)
+  # generate n_trials samples
+  data <- sample(x=1:365, size=n*n_trials, replace=TRUE)
 
-# rows are trials, columns are persons.  Each row is a randomly generated
-# group of birthdays for n persons
-data_matrix <- matrix(data, ncol=n)
+  # rows are trials, columns are persons.  Each row is a randomly generated
+  # group of birthdays for n persons
+  data_matrix <- matrix(data, ncol=n)
 
-# count how many birthdays are shared by at least two people in each row.
-# table() produces counts for each birthday in the row, and sum()
-# counts how many birthdays are shared by at least two people.
-matching_birthdays <- apply(X=data_matrix, 
-                            MARGIN=1, 
-                            FUN=function(x)  sum( table(x) >= 2 ))
+  # count how many birthdays are shared by at least two people in each row.
+  # table() produces counts for each birthday in the row, and sum()
+  # counts how many birthdays are shared by at least two people.
+  matching_birthdays <- apply(X=data_matrix, 
+                              MARGIN=1, 
+                              FUN=function(x)  sum( table(x) >= min_people_sharing ))
 
+  hist(matching_birthdays, probability=TRUE)
   
-# plot relative histogram of birthday distribution
-hist(matching_birthdays,
+  # plot relative histogram of birthday distribution
+  hist(matching_birthdays,
      main = 'Histogram of Shared Birthdays',
      xlab = paste('how many birthdays are shared for a group of', n),
      ylab = 'relative frequency', 
-     freq = FALSE,
+     probability = TRUE,
+     breaks = min(matching_birthdays) : (max(matching_birthdays) + 1) - .5,
      col = 'grey')
 
+  # probability of x or more matching birthdays in a group of size n
+  return( mean(matching_birthdays >= min_shared_birthdays) )
 
-# probability of three or more matching birthdays in a group of size n
-mean(matching_birthdays >= 3)
+}
+
+# standard birthday problem (probability of at least one shared birthday in
+# a group of 23) estimated with 10,000 draws
+birthday_problem()
+
+# 2014 DSSG birthday problem (probability of at least three shared birthdays
+# in a group of 63) estimated with 100,000 draws
+birthday_problem(n=63, n_trials=10000, min_shared_birthdays=3, min_people_sharing=2)
+
+# given a total group of 50 people, what is the probability of at least one 
+# subgroup of three people sharing a birthday?
+birthday_problem(n=50, n_trials=10000, min_shared_birthdays=1, min_people_sharing=3)
